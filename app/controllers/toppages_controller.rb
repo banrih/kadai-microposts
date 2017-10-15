b4a43738efd77e63387d107091cb2e2759ab996c
banrihayashi <banrihayashi@gmail.com>
2017-10-15 11:29:21 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,8 +2,8 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @user = current_user
-      @micropost = current_user.microposts.build
-      @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
+      @micropost = current_user.microposts.build # form_for 用
+      @microposts = current_user.feed_microposts.order('created_at DESC').page(params[:page]) # 一覧表示用
     end
   end
 end
